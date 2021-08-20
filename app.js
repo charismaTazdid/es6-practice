9902b71da1ce9cdd4ca84c8d582cb9a72a94ebbf
Raihan Tajdid <raihantajdid@gmail.com>
2021-08-20 21:28:46 +0600

    hurrrreeee I am practice javascript es 6

---
+++ app.js
@@ -0,0 +1,28 @@
+// variable var
+// constant const 
+const goverment = "Aowami legue";
+
+
+const babu = "Sapnur";
+
+
+let alurDam = 30;
+
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i)
+
+
+
+
+
+
+
+
+
+for(var i = 0; i<30; i++){
+    
+}
