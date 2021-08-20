01c890e715722696b7213f6e47934656f37fa660
Raihan Tajdid <raihantajdid@gmail.com>
2021-08-20 22:10:06 +0600

    default parametter file added

---
+++ functionDefault.js
@@ -0,0 +1,7 @@
+function addNumber(num1, num2 = 0){
+   
+    return num1 + num2;
+}
+
+const result = addNumber(40);
+console.log(result)
---
+++ letconst.js
@@ -0,0 +1,16 @@
+const friendsName = ['korim ullah', 'solim ulllah']
+
+friendsName.push(9090)
+
+
+const player = {
+    id: 40,
+    name: 'sakib al hasan',
+    sellary: 4000000, 
+    marriedStatus: true,
+
+}
+
+player.name= "mashrafi bin mortoza"
+
+console.log(player)
